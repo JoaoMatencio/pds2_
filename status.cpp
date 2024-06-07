12acbb7ca538e623a95f9f2fc0917ff74e7d9c70
JoaoMatencio <joaomatencio@gmail.com>
2024-06-07 18:18:33 -0300

    FEAT: add Status class with methods and attributes

---
+++ status.cpp
@@ -0,0 +1,33 @@
+#include "Status.hpp"
+#include <iostream>
+
+// Construtor
+Status::Status() : portaAberta(false), log("") {}
+
+// Métodos
+void Status::escreverLog(const std::string &mensagem) {
+    log += mensagem + "\n";
+}
+
+std::string Status::lerLog() const {
+    return log;
+}
+
+void Status::modificarLog(const std::string &novoLog) {
+    log = novoLog;
+}
+
+bool Status::retornarStatus() const {
+    return portaAberta;
+}
+
+// Métodos auxiliares
+void Status::abrirPorta() {
+    portaAberta = true;
+    escreverLog("Porta aberta.");
+}
+
+void Status::fecharPorta() {
+    portaAberta = false;
+    escreverLog("Porta fechada.");
+}
