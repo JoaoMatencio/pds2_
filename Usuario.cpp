201e8618ad218622f010f324d7ea659289160a50
Yan Ribeiro <xillonbja@hotmail.com>
2024-06-14 00:37:03 -0300

    ClasseUserV4

--- Usuario.cpp
+++ Usuario.cpp
@@ -1,32 +1,8 @@
-#include "usuarios.hpp"
-#include <iostream>
+#include "usuario.hpp"
 
-// Adicionar um novo usuário
-void Usuarios::adicionarUsuario(int userID, const std::string &nome, const std::map<int, Horarios> &acesso) {
-    Usuario novoUsuario;
-    novoUsuario.userID = userID;
-    novoUsuario.nome = nome;
-    novoUsuario.acesso = acesso;
-    usuarios.push_back(novoUsuario);
-}
+// Métodos
+user::user(int id, std::string nome) : userID(id), _nome(nome) {}
 
-// Remover um usuário existente com base no userID
-void Usuarios::removerUsuario(int userID) {
-    for (auto it = usuarios.begin(); it != usuarios.end(); ++it) {
-        if (it->userID == userID) {
-            usuarios.erase(it);
-            break;
-        }
-    }
-}
-
-// Modificar o acesso de um usuário
-void Usuarios::atualizarAcessoUsuario(int userID, const std::map<int, Horarios> &novoAcesso) {
-    for (auto &usuario : usuarios) {
-        if (usuario.userID == userID) {
-            usuario.acesso = novoAcesso;
-            return;
-        }
-    }
-    std::cout << "Usuário com ID " << userID << " não encontrado." << std::endl;
+void user::adicionarArea(area* novaArea) {
+    areas.insert(std::make_pair(novaArea->get_id(), novaArea));
 }
