bb445f1a695a233690114c060d07d290ffdc460a
jvcerbino <joaovcerbino@gmail.com>
2024-06-28 22:23:24 -0300

    Update permission.cpp

--- permission.cpp
+++ permission.cpp
@@ -3,20 +3,79 @@
 #include "area.hpp"
 #include "date.hpp"
 
-Permission::Permission() : acesso(false), monitoramento("") {}
+Permission::Permission() : acesso(0), monitoramento("") {}
 
+bool permission::acessoCedido(bool acesso, int areaID, int WeekDay, int PeriodDay, std::string capacity, std::string *monitoramento){
 
-alterarAtributos(){
+  while(acesso == 0){
+    switch(capacity){
 
+      case "PARENT":
+      acesso = 1;
+      return acesso;
+      break;
+
+      case "TEEN":
+      //coloquei um areaID arbitrario como 15, que seria o escritorio, por exemplo.
+      if(areaID != 15){
+        acesso = 1
+        return acesso
+      }
+      else{
+        return acesso;
+      }
+      break;
+
+      case "CHILDREN":
+        bool aux = 1;
+        switch(areaID){
+
+          // num possivel caso de sala de jogos
+          case 5:
+          if(PeriodDay == 1){
+          aux = 0;
+          }
+          break;
+
+          // casa de maquinas
+          case 7:
+          aux = 0;
+          break;
+
+          //exterior da casa
+          case 10:
+          if(PeriodDay == 1){
+            aux = 0;
+          }
+          break;
+
+          case 15:
+          aux = 0;
+          break;
+
+          default:
+          break;
+       }
+
+      acesso = aux;
+      return acesso;
+      break;
+
+      case "HOUSEKEEPER":
+      if(WeekDay == 3 && PeriodDay == 8){
+        acesso = 1;
+        return acesso;
+      }
+      else{
+        return acesso;
+      }
+      break;
+
+      default:
+      break;
+  }
 }
-
-bool permission::acessoCedido(bool acesso, int userID, int areaID, std::string diaSemana, std::string periodoDia){
-  
-}
-
-std::string permission::monitamentoPermission(std::string monitoramento, int userID, int areaID, std::string diaSemana, std::string periodoDia){
   std::ostringstream aux;
-  aux << "UserID: " << userID << "AreaID: " << areaID << "Dia :" << diaSemana << "Horario :" << periodoDia;
+  aux << "UserID: " << userID << "AreaID: " << areaID << "Dia :" << WeekDay << "Horario :" << PeriodDay << "Acesso" << acesso;
   monitoramento = aux;
-  return monitoramento;
 }
