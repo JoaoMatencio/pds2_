a2692b0c2c53d764f117854e6d7dbaa736bdee6d
JoaoMatencio <joaomatencio@gmail.com>
2024-07-06 00:48:24 -0300

    FIX: Remove incorrect Inputs

--- PDS2_/main.cpp
+++ PDS2_/main.cpp
@@ -1,10 +1,6 @@
 #include "administrator.hpp"
-#include "area.hpp"
 #include "login.hpp"
 #include "user.hpp"
-#include <map>
-#include <iostream>
-#include <string>
 
 int main() {
     bool stop = false;
@@ -16,8 +12,13 @@ int main() {
         if (isAdmin(nomeUsuario, senhaUsuario)) {
             Administrator admin;
             admin.switchCaseAdmin(getOption(), usuariosConfigurados);
-        } else {
+        } 
+        else if (usuariosConfigurados.find(nomeUsuario) != usuariosConfigurados.end()) 
+        {   
             // Implementação do código para conferencia de autorizações e acessos efetivos.
+        }
+        else {
+            std::cout << "Usuário não cadastrado." << std::endl;
         }
     }
     return 0;
